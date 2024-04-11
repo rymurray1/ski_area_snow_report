04a01deb04a37de31c5f1f9637025e3f38a58c62
rymurray1 <ryanmmurray8@gmail.com>
2024-04-11 01:40:31 -0500

    Added style and index files, updated app file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from api_analysis import read_max_data, read_data
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def display():
     df = read_data()
-    max_snow_val = str(read_max_data(df)["snowfall_sum"].round(2))
-    max_resort = str(read_max_data(df)["resort"])
-    output = display_val = f"The best resort to ski at this week is <strong>{max_resort}</strong> with <strong>{max_snow_val}</strong> inches of anticipated snow over the next 7 days."
-    return output
+    max_snow_val = read_max_data(df)["snowfall_sum"].round(2)
+    max_resort = read_max_data(df)["resort"]
+    return render_template('index.html', max_resort=max_resort, max_snow_val=max_snow_val)
