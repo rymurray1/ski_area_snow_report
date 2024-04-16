98deb98d11578f9391151501125d999d98510f8e
rymurray1 <ryanmmurray8@gmail.com>
2024-04-15 23:37:01 -0500

    added summer page to site

--- app.py
+++ app.py
@@ -15,9 +15,14 @@ scheduler = BackgroundScheduler(daemon=True)
 scheduler.add_job(scheduled_task, 'interval', hours=6)
 scheduler.start()
 
-@app.route('/')
+@app.route('/snow')
 def display():
     df = read_data()
     max_snow_val = read_max_data(df)[0].round(2)
     max_resort = read_max_data(df)[1].title()
     return render_template('index.html', max_resort=max_resort, max_snow_val=max_snow_val)
+
+
+@app.route('/summer')
+def display():
+    return render_template('summer.html')
